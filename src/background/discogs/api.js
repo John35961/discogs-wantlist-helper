76d7945b2b80d514372105ba2e0351d75e0ed2ba
Jean <contact@jeanguillet.com>
2025-10-22 18:56:33 +0200

    added searchForReleases

--- src/background/discogs/api.js
+++ src/background/discogs/api.js
@@ -19,6 +19,34 @@ const getUser = async (username) => {
   return data;
 };
 
+const searchForReleases = async (query) => {
+  const stored = await chrome.storage.local.get(['accessToken', 'accessTokenSecret']);
+  const accessToken = stored.accessToken;
+  const accessTokenSecret = stored.accessTokenSecret;
+
+  const url = new URL(`${DISCOGS_API_WRAPPER_BASE_URL}/database/search`);
+  url.searchParams.set('accessToken', accessToken);
+  url.searchParams.set('accessTokenSecret', accessTokenSecret);
+  url.searchParams.set('q', encodeURIComponent(query));
+
+  const requestData = {
+    url: url,
+    method: 'GET'
+  };
+
+  const res = await fetch(requestData.url, {
+    method: requestData.method,
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.error);
+  };
+
+  return data;
+}
+
 const addToWantlist = async (releaseId) => {
   const stored = await chrome.storage.local.get(['accessToken', 'accessTokenSecret', 'username']);
   const accessToken = stored.accessToken;
@@ -50,4 +78,4 @@ const addToWantlist = async (releaseId) => {
   return data;
 };
 
-export { getUser, addToWantlist };
+export { getUser, searchForReleases, addToWantlist };
