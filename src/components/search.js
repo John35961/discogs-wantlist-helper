eb53b6673fd1609d4c8c21d10bea3f9a50c9f23a
Jean <contact@jeanguillet.com>
2025-11-19 21:41:34 +0100

    persisted search

--- src/components/search.js
+++ src/components/search.js
@@ -1,14 +1,24 @@
+const MAX_STORED_RESULTS = 50;
+
 export default function () {
   return {
+    results: [],
     fetching: false,
     hasNextPage: false,
     nextPage: null,
     remainingResultsCount: 0,
 
+    async init() {
+      const stored = await chrome.storage.local.get(['query', 'results', 'hasNextPage', 'nextPage', 'remainingResultsCount']);
+      this.query = stored.query;
+      this.results = stored.results;
+      this.hasNextPage = stored.hasNextPage;
+      this.nextPage = stored.nextPage;
+      this.remainingResultsCount = stored.remainingResultsCount;
+    },
+
     async handleSearch() {
       this.fetching = true;
-      this.results = [];
-      this.resultsCount = 0;
       this.message = '';
       this.error = '';
 
@@ -17,6 +27,7 @@ export default function () {
       if (!query) {
         this.error = 'Search query is missing';
         this.fetching = false;
+        this.results = null;
         return;
       };
 
@@ -31,6 +42,14 @@ export default function () {
         this.hasNextPage = pagination.page < pagination.pages;
         this.nextPage = pagination.page + 1;
         this.remainingResultsCount = pagination.items - data.results.length;
+
+        await chrome.storage.local.set({
+          query: this.query,
+          results: data.results,
+          hasNextPage: this.hasNextPage,
+          nextPage: this.nextPage,
+          remainingResultsCount: this.remainingResultsCount,
+        });
       } else {
         this.error = response.error;
       };
@@ -50,6 +69,13 @@ export default function () {
         this.hasNextPage = pagination.page < pagination.pages;
         this.nextPage = data.pagination.page + 1;
         this.remainingResultsCount = pagination.items - this.results.length;
+
+        await chrome.storage.local.set({
+          results: this.results.slice(0, MAX_STORED_RESULTS),
+          hasNextPage: this.hasNextPage,
+          nextPage: this.nextPage,
+          remainingResultsCount: this.remainingResultsCount,
+        });
       } else {
         this.error = response.error;
       };
